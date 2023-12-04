47c89a4bf8624a302ea5d7848f0049a3315ccfcf
Arkadiy Byts <bytsarkadiy@gmail.com>
2023-12-04 02:15:11 +0100

    third task(first attempt)

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,58 @@
-function createPromise(position, delay) {
+console.log('script goes');
+import Notiflix from 'notiflix';
+
+const submitBtn = document.querySelector('button');
+//console.log(submitBtn);
+const firstDelay = document.querySelector('[name="delay"]');
+//console.log(firstDelay);
+const delayStep = document.querySelector('[name="step"]');
+//console.log(delayStep);
+const amount = document.querySelector('[name="amount"]');
+//console.log(amount);
+const shouldResolve = Math.random() > 0.3;
+
+function createPromise() {
   const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
   if (shouldResolve) {
-    // Fulfill
+    resolve()
   } else {
-    // Reject
+    reject()
   }
+})
 }
+
+  submitBtn.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    console.log(+firstDelay.value + 100);
+    console.log(delayStep.value);
+    console.log(amount.value);
+    
+    if (amount.value > 0) {
+      setTimeout(() => { 
+        let positionSetter = 1;
+        let delaySetter = +firstDelay.value;
+        const interval = setInterval(() => {
+          
+          createPromise()
+          .then(() => {
+            Notiflix.Notify.success(`✅ Fulfilled promise ${positionSetter} in ${delaySetter}ms`);
+            
+          })
+          .catch(() => {
+            Notiflix.Notify.failure(`❌ Rejected promise ${positionSetter} in ${delaySetter}ms`);
+            
+          })
+          .finally(() => {         
+            if (positionSetter === +amount.value) {
+              clearInterval(interval);
+              console.log('All repetitions completed.');
+            }
+            positionSetter += 1;
+            delaySetter += +delayStep.value;
+             
+          });
+        }, +delayStep.value)
+      }, +firstDelay.value)
+    }
+  });
