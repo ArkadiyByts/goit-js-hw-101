3841d01d77cd0b1aa3a0fe8e5a91f42d6d85ccca
Arkadiy Byts <bytsarkadiy@gmail.com>
2024-02-22 00:12:16 +0100

    first attempt

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,3 +1,33 @@
-export default function fetchBreeds() {
-    return axios.get('/breeds').then(({ data }) => data);
-}
+
+import axios from "axios";
+import Notiflix from 'notiflix';
+// axios.defaults.headers.common['x-api-key'] = 'live_xV4P8GODSToxWGFcWBRktHi2HWYRaBbjERo5IeqTpaRTLrNpRtRsvSqBclkITomc';
+// axios.defaults.baseURL = "https://api.thecatapi.com/v1";
+
+// export function fetchBreeds() {
+//     return axios.get('/breeds').then(({ data }) => data);
+// }
+
+// export function fetchCatByBreed(breedId) {
+//     return axios.get(`/images/search?breed_ids=${breedId}12`).then(({ data }) => data);
+// }
+
+
+
+export const fetchBreeds = () => {
+    axios.defaults.headers.common['x-api-key'] = 
+        'live_xV4P8GODSToxWGFcWBRktHi2HWYRaBbjERo5IeqTpaRTLrNpRtRsvSqBclkITomc';
+
+    return axios.get(`https://api.thecatapi.com/v1/breeds`)
+        .then(res => res.data)
+        .catch(error => {
+            Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+          });
+        
+};
+
+export const fetchCatByBreed = breedId => {
+   return axios
+        .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+        .then(res => res.data);
+};
--- src/index.js
+++ src/index.js
@@ -1,22 +1,70 @@
-console.log('goes');
+console.log('gooos');
 
-import axios from "axios";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
-axios.defaults.headers.common['x-api-key'] = 'live_xV4P8GODSToxWGFcWBRktHi2HWYRaBbjERo5IeqTpaRTLrNpRtRsvSqBclkITomc';
-axios.defaults.baseURL = "https://api.thecatapi.com/v1";
+const breedSelect = document.querySelector(".breed-select");
+const catInfo = document.querySelector(".cat-info");
+const loader = document.querySelector(".loader");
 
-function fetchBreeds() {
-    return axios.get('/breeds').then((responce) => {
-        return responce.data;
-    })
-}
-
-const catter = document.querySelector('.cat-info');
-
-fetchBreeds().then((data) => {
-    catter.insertAdjacentHTML('afterbegin', data);
+const slim = new SlimSelect({
+    select: breedSelect,
+    placeholder: 'Choose a Cat'
 })
 
+  try {
+    loader.classList.remove('invisible');
+    fetchBreeds().then(data => {
+      renderSelect(data);
+      // Refresh Slim Select after updating the options
+      slim.setData([{ text: 'Choose a Cat', value: '' }, ...data.map(({ id, name }) => ({ value: id, text: name }))]);
+  });
+  } catch (error) {
+    console.log(error);
+  }
+
+
+function renderSelect(breeds) {
+
+  const defaultOption = '<option value="">Choose a Cat</option>';
+
+    const markup = breeds
+      .map(({ id, name }) => {
+        return `<option value="${id}">${name}</option>`;
+      })
+      .join("");
+    breedSelect.insertAdjacentHTML("beforeend", markup);
+    loader.classList.add('invisible');
+}
+
+breedSelect.addEventListener('change', e => {
+  const selectedValue = e.target.value;
+  if (selectedValue === '') {
+    catInfo.innerHTML = '';
+    return;
+}
+    loader.classList.remove('invisible');
+    fetchCatByBreed(selectedValue)
+        .then(data => renderCat(data[0]));
+    catInfo.innerHTML = '';    
+});
+    
+
+function renderCat(catData) {
+    const { url } = catData;
+    const { description, name, temperament } = catData.breeds[0];
+    catInfo.innerHTML = 
+    `<div class="img-container">
+    <img id="cat-pic" src="${url}" />
+    </div>
+    <div class="info-container">
+    <h2>${name}</h2>
+    <p>${description}</p>
+    <p><span id="span-cat">Temperament: </span>${temperament}</p>
+    </div>`;
+    loader.classList.add('invisible');
+}
 
 
 
@@ -29,65 +77,64 @@ fetchBreeds().then((data) => {
 
 
 
-// let page = 1;
+
+
+// import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+// import './css/styles.css';
+// import Notiflix from 'notiflix';
+
+// Notiflix.Notify.init({
+// timeout: 2000,
+// });
 
 // const catter = document.querySelector('.cat-info');
-// const prev = document.querySelector('#prev');
-// const next = document.querySelector('#next');
-// const pageInfo = document.querySelector('#page');
 
-// pageInfo.innerText = page;
+// /** @type {HTMLSelectElement || null} */
+// const body = document.querySelector('body');
+// const breeds = document.querySelector('.breed-select');
+// const content = document.querySelector('#content');
 
-// if (page === 1) {
-//     prev.setAttribute('disabled', 'disabled');
-// }
+// window.addEventListener('load', () => {
+    
+//         content.classList.add('invisible-refresh');
+//         Notiflix.Notify.info('Loading data, please wait...');
+//         console.log('1sec');
 
-// prev.addEventListener('click', prevPage);
-// next.addEventListener('click', nextPage);
+//     // Remove the 'invisible-refresh' class after 2 seconds
+//     setTimeout(() => {
+//         content.classList.remove('invisible-refresh');
+//         console.log('2sec');
+//     }, 2300);
+// });
 
-// function nextPage() {
-//     fetchAndRender(++page);
-//     pageInfo.innerText = page;
-//     togglePrevButton();
-// }
+// fetchBreeds().then((data) => {
+//     const html = data.map((breed) => `<option value="${breed.id}">${breed.name}</option>`)
+//     breeds.innerHTML = html;
+// });
 
-// function prevPage() {
-//     fetchAndRender(--page);
-//     pageInfo.innerText = page;
-//     togglePrevButton();
-// }
+// breeds.addEventListener('change', (ev) => {
+//     const breedId = ev.target.value;
+//     catter.innerHTML = '';
+//     //Notiflix.Notify.info('Content is loading...');
+//     //breeds.classList.add('invisible');
+//     catter.classList.add('invisible');
 
-// function togglePrevButton() {
-//     if (page === 1) {
-//         prev.setAttribute('disabled', 'disabled');
-//     } else {
-//         prev.removeAttribute('disabled');
-//     } 
-// }
+//     fetchCatByBreed(breedId).then((cats) => {
+//             //catter.innerHTML = JSON.stringify(cats);
+            
+//             const array = cats.map((cat) => `<div class="img-container"><img id="cat-pic" src="${cat.url}" /></div><div class="info-container"><h2>${cat.breeds[0].name}</h2><p>${cat.breeds[0].description}</p><p><span id="span-cat">Temperament: </span>${cat.breeds[0].temperament}</p></div>`);
+//             if (array.length !== 0) {
+//                 Notiflix.Notify.info('Content is loading...');
+//                 catter.innerHTML = array.join("");
+//             } else {
+//                 Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+//             }
+//         });
+    
+//     setTimeout(() => {
+        
+        
+//         catter.classList.remove('invisible');
+//     }, 1500);
+// });
 
-// function fetchAndRender(currentPage) {
-   
-//     const url = "https://jsonplaceholder.typicode.com/posts";
-//     const searchParams = new URLSearchParams({
-
-//         _limit: 10,
-//         _page: currentPage
-//     });
-
-//     fetch(`${url}?${searchParams.toString()}`)
-//     .then((response) => {
-//         return response.json();
-//     })
-
-//     .then(rerender)
-//     .catch((error) => {
-//         catter.insertAdjacentHTML(error);
-//     });
-// }
-
-// fetchAndRender(page);
-
-// function rerender(data) {
-//     const html = data.map((elem) => `<div class='post'>[#${elem.id}] ${elem.title}</div>`)
-//     catter.innerHTML = html.join("<br />"); 
-// }
